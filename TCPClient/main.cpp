e61aa57f258521d8c384f79e09bd5d5abfaa7c33
Your Name <you@example.com>
2022-05-27 15:40:10 +0300

    Добавьте файлы проекта.

---
+++ TCPClient/main.cpp
@@ -0,0 +1,87 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+
+// В опциях проекта, в разделе Linker, в пункте Additional Dependancies укажите Ws2_32.lib
+#pragma comment(lib, "Ws2_32.lib")
+
+int  main(void)
+{
+	WSADATA wsaData;
+	SOCKET ConnectSocket;  // впускающий сокет и сокет для клиентов
+	sockaddr_in ServerAddr;  // это будет адрес сервера
+	int err, maxlen = 1024;  // код ошибки и размер буферов
+	char recvbuf [10];  // буфер приема
+	char query [512];  // буфер отправки
+	string server;
+	std::string myString;
+	int port;
+
+	std::cout << "Please Enter address Server" << std::endl;
+	getline(cin, server);
+	std::cout << "Please Enter Port" << std::endl;
+	cin >> port;
+
+	// Initialize Winsock
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	// Connect to server
+	ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	ServerAddr.sin_family = AF_INET;
+	//ServerAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ServerAddr.sin_addr.s_addr = inet_addr(server.c_str());
+	//ServerAddr.sin_port = htons(12345);
+	ServerAddr.sin_port = htons(port);
+
+	err = connect(ConnectSocket, (sockaddr*)&ServerAddr, sizeof(ServerAddr));
+
+	if (err == SOCKET_ERROR) {
+		printf("connect failed: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Server OK" << endl;
+
+//	_snprintf_s(query, maxlen, maxlen, "CALC * 12 6\n");
+	// отправляем запрос на сервер
+	//send(ConnectSocket, query, strlen(query), 0);
+	//printf("Sent: %s\n", query);
+
+	// получаем результат
+//	while (true) {
+
+		int nDataLength;
+		while (true)
+		{
+			nDataLength = recv(ConnectSocket, recvbuf, sizeof(recvbuf), 0);
+			if (nDataLength > 0)
+				myString.append(recvbuf, nDataLength);
+			else
+				std::cout << myString << "\n";
+
+
+		}
+		/*while ((nDataLength = recv(ConnectSocket, recvbuf, sizeof(recvbuf), 0)) > 0) {
+			myString.append(recvbuf, nDataLength);
+			
+		}
+		std::cout << myString << "\n";*/
+
+		
+		
+	//}
+
+	// shutdown the connection since we're done
+	closesocket(ConnectSocket);
+
+}
